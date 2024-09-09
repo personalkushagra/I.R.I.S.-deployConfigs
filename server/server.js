d90c1401dc8218e8f5797f009e50eeb833870dfa
Kushagra Anit Singh <kushagraa.n@gmail.com>
2024-09-09 17:15:32 +0530

    url

--- server/server.js
+++ server/server.js
@@ -2,13 +2,9 @@ import mysql from 'mysql2';
 import 'dotenv/config';
 import Razorpay from "razorpay";
 
-// Database connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSOLPORT}/${process.env.MYSQLDATABASE}`
+
+const db = mysql.createConnection(urlDB);
 
 db.connect((err) => {
   if (err) {
