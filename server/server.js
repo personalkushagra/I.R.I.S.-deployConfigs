0651d18512d0bf943f2c7280a77e7afc58f97818
Kushagra Anit Singh <kushagraa.n@gmail.com>
2024-09-10 01:32:27 +0530

    1:32

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import Razorpay from "razorpay";
 
 //const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSOLPORT}/${process.env.MYSQLDATABASE}`
 
-const urlDB = `mysql://root:WRiXiuOncwXiCayINfmceCElHWYWVfQT@mysql.railway.internal:3306/railway`
+const urlDB = `mysql://root:WRiXiuOncwXiCayINfmceCElHWYWVfQT@iris-deployconfigs.railway.internal:3306/railway`
 
 const db = mysql.createConnection(urlDB);
 
